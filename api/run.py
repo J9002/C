f9be9b6fbefe86d24613052474f0d8aaa44c6523
J9002 <JimmyLightning936@outlook.com>
2026-05-18 19:10:19 +0000

    adasda

--- api/run.py
+++ api/run.py
@@ -4,10 +4,12 @@ def get(lines):
     terms = []
     for i in lines:
         i = i.strip()
+        i = i.replace("^", "**")
         if i[0] == 'x':
             i = '1*' + i
-        if not i.endswith("x"):
-            i = i.replace('x','x**',1)
+        if "x**" not in i:
+            if not i.endswith("x"):
+                i = i.replace('x','x**',1)
         for c in range(1, len(i)):
             if i[c] == 'x' and i[c-1].isdigit():
                 i = i[:c] + '*' + i[c:]
@@ -21,13 +23,17 @@ def calculate():
     f = sum(exprsf)
     d = sum(exprsd)
     q, r = div(f, d)
-    with open("output.txt", "w") as f2:
-        f2.write(str(q).replace("**", "^"))
+    out = str(q).replace("**", "^")
+    if r != 0:
+        out += " + " + str(r).replace("**", "^") + "/(" + str(d).replace("**", "^") + ")"
+    with open("/workspaces/C/api/output.txt", "w") as f2:
+        f2.write(out)
 
 def main():
+    
     global final
     global divisor
-    with open("input.txt", r) as f:
+    with open("/workspaces/C/api/input.txt") as f:
         lines = [x.strip() for x in f.readlines()]
     split = lines.index("")
     final = get(lines[:split])
---
+++ api/write.py
@@ -0,0 +1,6 @@
+def write(file, text):
+    with open(f"workspaces/C/api/{file}", "w") as f:
+        f.write(text)
+        f.close
+
+write("output.txt", "jon")
