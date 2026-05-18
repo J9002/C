b7db9e0d285c482402da97388b1e5c5b6edfd44f
J9002 <JimmyLightning936@outlook.com>
2026-05-18 18:51:49 +0000

    jscjsb

--- api/run.py
+++ api/run.py
@@ -1,7 +1,37 @@
-from http.server import BaseHTTPRequestHandler
+from sympy import *
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Python is running!")
+def get(lines):
+    terms = []
+    for i in lines:
+        i = i.strip()
+        if i[0] == 'x':
+            i = '1*' + i
+        if not i.endswith("x"):
+            i = i.replace('x','x**',1)
+        for c in range(1, len(i)):
+            if i[c] == 'x' and i[c-1].isdigit():
+                i = i[:c] + '*' + i[c:]
+                break
+        terms.append(i)
+    return terms
+
+def calculate():
+    exprsf = [sympify(s) for s in final]
+    exprsd = [sympify(s) for s in divisor]
+    f = sum(exprsf)
+    d = sum(exprsd)
+    q, r = div(f, d)
+    with open("output.txt", "w") as f2:
+        f2.write(str(q).replace("**", "^"))
+
+def main():
+    global final
+    global divisor
+    with open("input.txt", r) as f:
+        lines = [x.strip() for x in f.readlines()]
+    split = lines.index("")
+    final = get(lines[:split])
+    divisor = get(lines[split+1:])
+    calculate()
+
+main()
