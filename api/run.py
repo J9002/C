faa9e3064e660275e4d56330853516710de496f8
J9002 <JimmyLightning936@outlook.com>
2026-05-18 18:16:17 +0000

    sjbdjbs

--- api/run.py
+++ api/run.py
@@ -1,5 +1,7 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Python is running!"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Python is running!")
